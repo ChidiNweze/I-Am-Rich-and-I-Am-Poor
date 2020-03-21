f62195d2d6345c9bee1fd46972a280fd3a832e63
Chidinma Umenwofor-Nweze <57571222+ChidiNweze@users.noreply.github.com>
2020-03-21 14:07:02 -0400

    Upload "I Am Poor" project to repository.

---
+++ I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Chidinma Umenwofor-Nweze on 2020-03-21.
+//  Copyright © 2020 Chidinma Umenwofor-Nweze. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
