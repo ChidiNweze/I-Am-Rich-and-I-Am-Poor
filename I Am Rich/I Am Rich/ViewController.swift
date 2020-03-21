5669aa74cd166a38451e7533ddedd607e7912166
Chidinma Umenwofor-Nweze <57571222+ChidiNweze@users.noreply.github.com>
2020-03-21 12:51:10 -0400

    Added files for "I Am Rich" app project

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Chidinma Umenwofor-Nweze on 2020-03-19.
+//  Copyright © 2020 Chidinma Umenwofor-Nweze. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
